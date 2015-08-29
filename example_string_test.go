a8e678992817d95f74367b44c14fbd14b52022e0
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-29 12:13:56 +0800

    Removed license header from godoc examples

--- example_string_test.go
+++ example_string_test.go
@@ -1,22 +1,3 @@
-/*
- * g2z - Zabbix module adapter for Go
- * Copyright (C) 2015 - Ryan Armstrong <ryan@cavaliercoder.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 package g2z_test
 
 import (
@@ -24,7 +5,7 @@ import (
 	"strings"
 )
 
-func Example_string() {
+func ExampleRegisterStringItem() {
 	panic("THIS_SHOULD_NEVER_HAPPEN")
 }
 
