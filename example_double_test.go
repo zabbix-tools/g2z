caf9dade053b6b862c98b96043f7da06ea9d0987
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-29 12:07:02 +0800

    Added example files

---
+++ example_double_test.go
@@ -0,0 +1,36 @@
+/*
+ * g2z - Zabbix module adapter for Go
+ * Copyright (C) 2015 - Ryan Armstrong <ryan@cavaliercoder.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package g2z_test
+
+import (
+	"github.com/cavaliercoder/g2z"
+)
+
+func main() {
+	panic("THIS_SHOULD_NEVER_HAPPEN")
+}
+
+func init() {
+	g2z.RegisterDoubleItem("go.double", "", Double)
+}
+
+func Double(request *g2z.AgentRequest) (float64, error) {
+	return 1.23456, nil
+}
---
+++ example_uint64_test.go
@@ -0,0 +1,32 @@
+/*
+ * g2z - Zabbix module adapter for Go
+ * Copyright (C) 2015 - Ryan Armstrong <ryan@cavaliercoder.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package g2z_test
+
+import (
+	"github.com/cavaliercoder/g2z"
+)
+
+func init() {
+	g2z.RegisterUint64Item("go.ping", "", Ping)
+}
+
+func Ping(request *g2z.AgentRequest) (uint64, error) {
+	return 1, nil
+}
